43ac22724d65a9c9379dfa0cb22738b4109b569a
Felipe19821982 <felipe.arq.10@gmail.com>
2024-10-23 11:08:29 -0300

    Mis cambios antes de actualizar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  showMenu: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      // Si la ruta actual es la página de inicio, ocultamos el menú
+      if (this.router.url === '/home') {
+        this.showMenu = false;
+      } else {
+        this.showMenu = true;
+      }
+    });
+  }
 }
